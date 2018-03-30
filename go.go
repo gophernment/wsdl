044ed3f0ebf666050e43408bfc6657e78b2c1c19
pallat <yod.pallat@gmail.com>
2018-03-30 20:50:57 +0700

    use default wsdl

--- go.go
+++ go.go
@@ -6,8 +6,6 @@ import (
 	"github.com/pallat/wsdl"
 )
 
-var serviceLocation = "http://localhost:1323/elastic"
-
 type Prototype interface {
 	Location() string
 	OperationName() string
@@ -24,90 +22,69 @@ type Type interface {
 }
 
 func WSDL(pro Prototype) (string, error) {
-	def := wsdl.Definitions{
-		WSDL:            "http://schemas.xmlsoap.org/wsdl/",
-		XSD:             "http://www.w3.org/2001/XMLSchema",
-		Soap:            "http://schemas.xmlsoap.org/wsdl/soap/",
-		TNS:             "urn:GOWSDL.wsdl",
-		TargetNamespace: "urn:GOWSDL.wsdl",
-		TypeAttr:        "urn:pack.GOWSDL_typedef.golang",
-		Documentation:   "",
-		Types: wsdl.Types{
-			Schemas: []wsdl.Schema{
-				{
-					AttributeFormDefault: "unqualified",
-					ElementFormDefault:   "qualified",
-					TargetNamespace:      "urn:pack.GOWSDL_typedef.golang",
-					Imports:              nil,
-					Elements: []wsdl.SchemaElement{
-						{
-							Element: wsdl.Element{
-								Name: pro.InputType().TypeName(),
-							},
-							ComplexType: wsdl.ComplexType{
-								Sequence: wsdl.Sequence{
-									Elements: NewElements(pro.InputType().SingleFields()),
-								},
-							},
-						},
-						{
-							Element: wsdl.Element{
-								Name: pro.OutputType().TypeName(),
-							},
-							ComplexType: wsdl.ComplexType{
-								Sequence: wsdl.Sequence{
-									Elements: NewElements(pro.OutputType().SingleFields()),
-								},
-							},
-						},
-						{
-							Element: wsdl.Element{
-								Name: pro.ErrorType().TypeName(),
-							},
-							ComplexType: wsdl.ComplexType{
-								Sequence: wsdl.Sequence{
-									Elements: NewElements(pro.ErrorType().SingleFields()),
-								},
-							},
-						},
-					},
+	def := wsdl.DefaultDefenitions
+
+	schemas := wsdl.DefaultSchema
+	schemas.Elements = []wsdl.SchemaElement{
+		{
+			Element: wsdl.Element{
+				Name: pro.InputType().TypeName(),
+			},
+			ComplexType: wsdl.ComplexType{
+				Sequence: wsdl.Sequence{
+					Elements: NewElements(pro.InputType().SingleFields()),
 				},
 			},
 		},
-		Messages: []wsdl.Message{
-			wsdl.NewMessage(pro.InputType().MessageName(), pro.InputType().TypeName()),
-			wsdl.NewMessage(pro.OutputType().MessageName(), pro.OutputType().TypeName()),
-			wsdl.NewMessage(pro.ErrorType().MessageName(), pro.ErrorType().TypeName()),
-		},
-		PortType: NewPortType([]wsdl.WSDLOperation{
-			{
-				Name:   pro.OperationName(),
-				Input:  wsdl.NewIOOperation(pro.InputType().MessageName(), ""),
-				Output: wsdl.NewIOOperation(pro.OutputType().MessageName(), ""),
-				Fault:  wsdl.NewFaultOperation(pro.ErrorType().MessageName(), pro.ErrorType().MessageName(), ""),
+		{
+			Element: wsdl.Element{
+				Name: pro.OutputType().TypeName(),
 			},
-		}),
-		Binding: NewBinding(wsdl.NewWSDLOperation(pro.OperationName(), pro.OperationName(), pro.ErrorType().MessageName())),
-		Service: wsdl.NewService(pro.Location()),
+			ComplexType: wsdl.ComplexType{
+				Sequence: wsdl.Sequence{
+					Elements: NewElements(pro.OutputType().SingleFields()),
+				},
+			},
+		},
+		{
+			Element: wsdl.Element{
+				Name: pro.ErrorType().TypeName(),
+			},
+			ComplexType: wsdl.ComplexType{
+				Sequence: wsdl.Sequence{
+					Elements: NewElements(pro.ErrorType().SingleFields()),
+				},
+			},
+		},
 	}
+
+	def.Types = wsdl.Types{
+		Schemas: []wsdl.Schema{schemas},
+	}
+
+	def.Messages = []wsdl.Message{
+		wsdl.NewMessage(pro.InputType().MessageName(), pro.InputType().TypeName()),
+		wsdl.NewMessage(pro.OutputType().MessageName(), pro.OutputType().TypeName()),
+		wsdl.NewMessage(pro.ErrorType().MessageName(), pro.ErrorType().TypeName()),
+	}
+
+	def.PortType.Operations = []wsdl.WSDLOperation{
+		{
+			Name:   pro.OperationName(),
+			Input:  wsdl.NewIOOperation(pro.InputType().MessageName(), ""),
+			Output: wsdl.NewIOOperation(pro.OutputType().MessageName(), ""),
+			Fault:  wsdl.NewFaultOperation(pro.ErrorType().MessageName(), pro.ErrorType().MessageName(), ""),
+		},
+	}
+
+	def.Binding.Operation = append(def.Binding.Operation, wsdl.NewWSDLOperation(pro.OperationName(), pro.OperationName(), pro.ErrorType().MessageName()))
+	def.Service = wsdl.NewService(pro.Location())
 
 	b, err := xml.MarshalIndent(&def, "", "    ")
 	if err != nil {
 		return "", err
 	}
 	return string(b), nil
-}
-
-func NewPortType(opers []wsdl.WSDLOperation) wsdl.PortType {
-	port := wsdl.DefaultPortType
-	port.Operations = opers
-	return port
-}
-
-func NewBinding(oper wsdl.WSDLOperation) wsdl.Binding {
-	binding := wsdl.DefaultBinding
-	binding.Operation = append(binding.Operation, oper)
-	return binding
 }
 
 func NewElements(names []string) []wsdl.Element {
