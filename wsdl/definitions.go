4e6c844e16aa1ecf95e37bf47cdc726ec4730cca
pallat <yod.pallat@gmail.com>
2019-02-17 00:20:39 +0700

    only types

---
+++ wsdl/definitions.go
@@ -0,0 +1,106 @@
+package wsdl
+
+import (
+	"encoding/xml"
+
+	"github.com/goknowgo/wsdl/xsd"
+)
+
+type Definitions struct {
+	XMLName         xml.Name  `xml:"definitions"`
+	Name            string    `xml:"name,attr"`
+	WSDL            string    `xml:"wsdl,attr"`
+	XSD             string    `xml:"xsd,attr"`
+	Soap            string    `xml:"soap11,attr"`
+	TNS             string    `xml:"tns,attr"`
+	TypeAttr        string    `xml:"gotype,attr"`
+	TargetNamespace string    `xml:"targetNamespace,attr"`
+	Documentation   string    `xml:"documentation"`
+	Types           Type      `xml:"types"`
+	Messages        []Message `xml:"message"`
+	PortType        PortType  `xml:"portType"`
+	Binding         []Binding `xml:"binding"`
+	Service         []Service `xml:"service"`
+}
+
+type Type struct {
+	Schemas []xsd.Schema `xml:"schema"`
+}
+
+type Message struct {
+	Name string `xml:"name,attr"`
+	Part Part   `xml:"part"`
+}
+
+type Part struct {
+	Name    string `xml:"name,attr"`
+	Element string `xml:"element,attr"`
+}
+
+type PortType struct {
+	Name       string              `xml:"name,attr"`
+	Operations []PortTypeOperation `xml:"operation"`
+}
+
+type PortTypeOperation struct {
+	Name   string                   `xml:"name,attr"`
+	Input  PortTypeOperationMessage `xml:"input"`
+	Output PortTypeOperationMessage `xml:"output"`
+	Fault  PortTypeOperationMessage `xml:"fault"`
+}
+
+type PortTypeOperationMessage struct {
+	Name    string `xml:"name,attr,omitempty`
+	Message string `xml:"message,attr"`
+}
+
+type Binding struct {
+	Name        string             `xml:"name,attr"`
+	Type        string             `xml:"type,attr"`
+	SoapBinding SoapBinding        `xml:"binding"`
+	Operations  []BindingOperation `xml:"operation"`
+}
+
+type SoapBinding struct {
+	XMLName   xml.Name `xml:"binding"`
+	Transport string   `xml:"transport,attr"`
+	Style     string   `xml:"style,attr"`
+}
+
+type BindingOperation struct {
+	Name          string        `xml:"name,attr"`
+	SoapOperation SoapOperation `xml:"operation"`
+	Input         SoapBodyIO    `xml:"input"`
+	Output        SoapBodyIO    `xml:"output"`
+	Fault         SoapBody      `xml:"fault>fault"`
+}
+
+type SoapOperation struct {
+	SoapAction string `xml:"soapAction,attr"`
+}
+
+type SoapBodyIO struct {
+	SoapBody SoapBody `xml:"body"`
+}
+
+type SoapBody struct {
+	Name string `xml:"name,attr,omitempty"`
+	Use  string `xml:"use,attr"`
+}
+
+type Service struct {
+	Name string      `xml:"name,attr"`
+	Port ServicePort `xml:"port"`
+}
+
+type ServicePort struct {
+	XMLName xml.Name       `xml:"port"`
+	Name    string         `xml:"name,attr"`
+	Binding string         `xml:"binding,attr"`
+	Address ServiceAddress `xml:"address"`
+}
+
+type ServiceAddress struct {
+	XMLName  xml.Name `xml:"address"`
+	Location string   `xml:"location,attr"`
+}
---
+++ xsd/xsd.go
@@ -0,0 +1,53 @@
+package xsd
+
+import (
+	"encoding/xml"
+)
+
+type Schema struct {
+	XMLName            xml.Name      `xml:"http://www.w3.org/2001/XMLSchema schema"`
+	TNS                string        `xml:"xmlns tns",attr`
+	XS                 string        `xml:"xmlns xs,attr"`
+	TargetNamespace    string        `xml:"targetNamespace,attr"`
+	ElementFormDefault string        `xml:"elementFormDefault,attr"`
+	Version            string        `xml:"version,attr"`
+	Imports            []Import      `xml:"http://www.w3.org/2001/XMLSchema import"`
+	Elements           []Element     `xml:"http://www.w3.org/2001/XMLSchema element"`
+	ComplexTypes       []ComplexType `xml:"http://www.w3.org/2001/XMLSchema complexType"`
+}
+
+type Element struct {
+	XMLName      xml.Name     `xml:"http://www.w3.org/2001/XMLSchema element"`
+	Type         string       `xml:"type,attr"`
+	Nillable     string       `xml:"nillable,attr"`
+	MinOccurs    string       `xml:"minOccurs,attr"`
+	MaxOccurs    string       `xml:"maxOccurs,attr"`
+	Form         string       `xml:"form,attr"`
+	Name         string       `xml:"name,attr"`
+	ComplexTypes *ComplexType `xml:"http://www.w3.org/2001/XMLSchema complexType"`
+}
+
+type ComplexType struct {
+	XMLName  xml.Name        `xml:"http://www.w3.org/2001/XMLSchema complexType"`
+	Name     string          `xml:"name,attr"`
+	Abstract bool            `xml:"abstract,attr"`
+	Sequence []Element       `xml:"sequence>element"`
+	Content  *ComplexContent `xml:"http://www.w3.org/2001/XMLSchema complexContent"`
+}
+
+type ComplexContent struct {
+	XMLName   xml.Name  `xml:"http://www.w3.org/2001/XMLSchema complexContent"`
+	Extension Extension `xml:"http://www.w3.org/2001/XMLSchema extension"`
+}
+
+type Extension struct {
+	XMLName  xml.Name  `xml:"http://www.w3.org/2001/XMLSchema extension"`
+	Base     string    `xml:"base,attr"`
+	Sequence []Element `xml:"sequence>element"`
+}
+
+type Import struct {
+	XMLName        xml.Name `xml:"http://www.w3.org/2001/XMLSchema import"`
+	SchemaLocation string   `xml:"schemaLocation,attr"`
+	Namespace      string   `xml:"namespace,attr"`
+}
